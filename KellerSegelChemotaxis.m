5cee98d0541398b696bd7a1ef32cb2c362ea1245
AndrewLKrause <42071282+AndrewLKrause@users.noreply.github.com>
2022-07-21 00:11:33 +0100

    Added chemotaxis code

--- KellerSegelChemotaxis.m
+++ KellerSegelChemotaxis.m
@@ -1,8 +1,7 @@
 clear; close all;
 
 
-a=0.075; b=0.029;
-d1 = 1;  d4=0.9; d3 = 0.6; d2 = 1;
+d1=1; d4=1; rho=1;chi0=4.01;
 
 %Domain length (needs to be reasonably large).
 L = 50;
@@ -13,23 +12,18 @@ N = 1000;
 dx = L/N;
 
 %Kinetic functions.
-f = @(u,v)(a-u+u.^2.*v);
-g = @(u,v)(b - u.^2.*v);
+f = @(u)(u-u.^2);
+g = @(u,v)(rho*u-v);
 
-%Steady state values.
-%uss = a+b; vss=b/(a+b)^2;
-
-%
-%
-
-%localized init condition
-uss = a+b; vss = b/uss^2;
+%Homogeneous background steady state
+uss = 1; vss = rho*uss;
 x = linspace(0,L,N);
-%initial conditions 
 
-spike = 3*exp(-1*x.^2);
-uss=uss+spike;
-vss=vss+spike;
+
+%initial conditions 
+rng(1);
+uss=uss*(1+1e-1*randn(N,1));
+vss=vss*(1+1e-1*randn(N,1));
 
 %uss = a+b+1e-2*randn(N,1);vss=b./((a+b).^2) +1e-2*randn(N,1);
 
@@ -46,7 +40,8 @@ lap = (1/dx)^2*lap;
 
 
 %Matlab ODE solvers need 
-F = @(t,U)[f(U(1:N),U(N+1:2*N))+d1*(1/dx)^2*Lap(U(1:N))+d2*(1/dx)^2*Lap(U(N+1:2*N));g(U(1:N),U(N+1:2*N))+d3*(1/dx)^2*Lap(U(1:N))+d4*(1/dx)^2*Lap(U(N+1:2*N))];
+F = @(t,U)[f(U(1:N))+d1*(1/dx)^2*Lap(U(1:N))-chi0*(1/(2*dx^2))*chmtxis(U(1:N),U(N+1:2*N));...,
+    g(U(1:N),U(N+1:2*N))+d4*(1/dx)^2*Lap(U(N+1:2*N))];
 
 %Get solution at times
 T = linspace(0,15000,20000);
@@ -56,7 +51,7 @@ LP = lap~=0; I = eye(N);
 JPattern = [LP, I+LP; I+LP, LP];
 
 %Solve the system using a stiff solver and low tolerances.
-odeparams = odeset('RelTol',1e-6,'AbsTol',1e-6,'JPattern',JPattern);
+odeparams = odeset('RelTol',1e-11,'AbsTol',1e-11,'JPattern',JPattern);
 [T, U] = ode15s(F,T,uinit,odeparams);
 
 u = U(:,1:N); v = U(:,N+1:2*N);
@@ -66,6 +61,13 @@ plot(u(end,:))
 
 
 function u = Lap(u)
-    %u(i-1)-2*u(i)+u(i+1)
     u = [u(2)-u(1); u(1:end-2)-2*u(2:end-1)+u(3:end);u(end-1)-u(end)];
+end
+
+
+%Version of (u_(i+1)+u_i)*(v_(i+1)-v_i)-(u_i+u_(i-1))*(v_i-v_(i-1))
+function chmtxis = chmtxis(u,v)
+    chmtxis = [(u(2)+u(1)).*(v(2)-v(1));...,
+        (u(3:end)+u(2:end-1)).*(v(3:end)-v(2:end-1))-(u(2:end-1)+u(1:end-2)).*(v(2:end-1)-v(1:end-2));...,
+        -(u(end)+u(end-1)).*(v(end)-v(end-1))];
 end
